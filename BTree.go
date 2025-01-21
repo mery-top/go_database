3c9f8181a63d8ee48a4bcca1e510e7e344589720
Meerthika <meerthikasrrajendrans@gmail.com>
2025-01-21 23:02:16 +0530

    Add struct and constructor

--- BTree.go
+++ BTree.go
@@ -1,6 +1,32 @@
 package main
-import "fmt"
 
-func btree(){
-	fmt.Println("THis is th tree")
+import (
+	"fmt"
+)
+
+type node struct{
+	t int
+	keys []int
+	children []int
+	is_leaf bool
+	next *node
+}
+
+func node_init(t int, keys []int,children []int, is_leaf bool ) node{
+	return node{
+		t:t,
+		keys: keys,
+		children: children,
+		is_leaf: is_leaf,
+		next: nil,
+	}
+}
+
+func init(t int){
+	var root int = node(t)
+	return root
+}
+
+func insert(node, key int){
+	
 }
