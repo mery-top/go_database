0b30585b6a053f0598409d37935dad4c28742aea
Meerthika <meerthikasrrajendrans@gmail.com>
2025-01-30 21:49:48 +0530

    Add insert and insert when not full functions

--- B+Tree.go
+++ B+Tree.go
@@ -31,5 +31,44 @@ func newBPlusTree(t int) *BPlusTree{
 }
 
 
+func (tree *BPlusTree) Insert(key int){
+	root:= tree.root
+	if len(root.keys) == 2*tree.t-1{
+		newRoot := newNode(tree.t, false)
+		newRoot.children = append(newRoot.children, root)
+		tree.splitChild(newRoot, 0)
+		tree.root = newRoot.next
+	}
+	tree.insertNonFull(tree.root, key)
+}
+
+
+func (tree * BPlusTree) insertNonFull(node *BPlusTreeNode, key int){
+	if node.isLeaf{
+		i := len(node.keys) -1
+		for i>=0 && key < node.key[i]{
+			i--
+		}
+		node.keys = append(node.keys[:i+1], append([]int{key}, node.keys[i+1:]...)...)
+	} else{
+		i:= len(node.keys) -1
+		for i>=0 && key < node.keys[i]{
+			i--
+		}
+		i++
+
+
+		if len(node.children[i].keys) == 2*tree.t-1{
+			tree.splitChild(node,i)
+			if key > node.keys[i] {
+				i++
+			}
+			tree.insertNonFull(node.children[i], key)
+		}
+	}
+
+
+}
+
 
 
